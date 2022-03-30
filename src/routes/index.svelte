c4f58a6339eabf2440654fc99993d1fc5d0332fb
Karpfador <24807827+Karpfador@users.noreply.github.com>
2022-03-31 00:27:23 +0200

    🐛 stuff works in theory, but UI isn't reactive at all

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,36 @@
-<script lang="ts">
-    import { graphql, query, type GetUser$input } from '$houdini'
-    import debounce from 'lodash/debounce'
+<script context="module" lang="ts">
+    //FIXME THIS IS NOT REACTIVE
+    let username = ''
+    // Without initial value, houdini throws an error
+    let tags = ['Shounen']
+    // These are up here in the first place because for some reason the
+    // "XVariables" Functions arent being found when outside of the module context
 
-    let username: string = ''
+    export function GetUserVariables(): GetUser['input'] {
+        if (username.length > 0) {
+            return this.error('Username not set')
+        }
+
+        return {
+            username,
+        }
+    }
+
+    export function GetAnimeVariables(): GetAnime['input'] {
+        if (tags.length === 0) {
+            return this.error('Tags not set')
+        }
+
+        return {
+            tags: [...tags],
+        }
+    }
+</script>
+
+<script lang="ts">
+    import { graphql, query } from '$houdini'
+    import type { GetUser, GetAnime } from '$houdini'
+    import debounce from 'lodash/debounce'
 
     const handleUsernameInput = debounce(e => {
         username = e.target.value
@@ -11,9 +39,8 @@
             userRefetch({ username })
         }
 
-        tags.clear()
-        tags = tags
-    }, 1000)
+        tags = []
+    }, 500)
 
     const {
         data: user,
@@ -40,44 +67,44 @@
         }
     `)
 
-    export function GetUserVariables(page): GetUser$input {
-        // make sure we recognize the value
-        if (!['active', 'completed'].includes(page.params.filter)) {
-            return this.error(400, 'invalid filter')
-        }
-
-        return {
-            username,
-        }
-    }
-
-    // const user = query<IUserResult>(GET_USER, { variables: { username: username ?? 'karpfador' } })
-
-    let tags: Set<string> = new Set()
-
-    const refetchAnime = debounce(() => {
-        if (tags.size > 0) {
-            // anime.refetch({ tags: Array.from(tags) })
-        }
-    }, 1000)
-
-    const handleTagToggle = tag => {
-        if (tags.has(tag)) {
-            tags.delete(tag)
+    const handleTagToggle = (tag: string) => {
+        if (tags.includes(tag)) {
+            tags = tags.filter(t => t !== tag)
         } else {
-            tags.add(tag)
+            tags = [...tags, tag]
         }
-
-        // Reassigning for rerender
-        tags = tags
 
         refetchAnime()
     }
 
-    // Initializing with a fixed value again to avoid issues thanks to lack of lazy query support
-    // const anime = query<IAnimeResult>(GET_ANIME, {
-    //     variables: { tags: ['Shounen'] },
-    // })
+    const {
+        data: anime,
+        error: animeError,
+        loading: animeLoading,
+        refetch: animeRefetch,
+    } = query(graphql`
+        query GetAnime($tags: [String!]!) {
+            Page(perPage: 20, page: 1) {
+                media(tag_in: $tags, type: ANIME, sort: SCORE_DESC) {
+                    id
+                    siteUrl
+                    title {
+                        english
+                        romaji
+                    }
+                    coverImage {
+                        medium
+                    }
+                }
+            }
+        }
+    `)
+
+    const refetchAnime = debounce(() => {
+        if (tags.length > 0) {
+            animeRefetch({ tags })
+        }
+    }, 500)
 </script>
 
 <svelte:head>
@@ -94,26 +121,21 @@
             on:input={handleUsernameInput}
         />
     </p>
-
+    {username}
     <div>
         {#if username}
             {#if $userLoading}
                 Loading User...
             {:else if $userError}
                 Error: {$user.error.message}
-            {:else if $user}
-                Found user <b>{$user.data.User.name}</b> with ID {$user.data.User.id}<br />
+            {:else if $user.User}
+                Found user <b>{$user.User.name}</b> with ID {$user.User.id}<br />
                 Your tags sorted by minutes watched:
                 <ul>
-                    {#each $user.data.User.statistics.anime.tags as tag}
+                    {#each $user.User.statistics.anime.tags as tag}
                         <li on:click={() => handleTagToggle(tag.tag.name)}>
-                            <input
-                                type="checkbox"
-                                checked={tags.has(tag.tag.name)}
-                                on:click={() => handleTagToggle(tag.tag.name)}
-                            />
-                            -->
-                            <!-- svelte-ignore a11y-invalid-attribute -->
+                            <input type="checkbox" checked={tags.includes(tag.tag.name)} />
+                            <!-- svelte-ignore a11y-invalid-attribute-->
                             <a href="javascript:;">
                                 {tag.tag.name} ({tag.minutesWatched} minutes)
                             </a>
@@ -125,23 +147,20 @@
     </div>
 
     <div>
-        {#if tags.size > 0}
-            <!-- {#if $anime.loading}
+        {#if tags.length > 0}
+            {tags}
+            {#if $animeLoading}
                 Loading Anime...
-            {:else if $anime.error}
+            {:else if $animeError}
                 Error: {$user.error.message}
-            {:else if $anime.data}
-                {#if $anime.data.Page.media.length > 0}
+            {:else if $anime && $user.User}
+                {#if $anime.Page.media.length > 0}
                     <p class="centered">The 20 highest rated anime with your selected tags</p>
                     <div class="animeGrid">
-                        {#each $anime.data.Page.media as anime}
+                        {#each $anime.Page.media as anime}
                             <div class="anime centered">
                                 <a href={anime.siteUrl} target="_blank">
-                                    <img
-                                        src={anime.coverImage.medium}
-                                        alt="anime
-                                cover"
-                                    />
+                                    <img src={anime.coverImage.medium} alt="anime cover" />
                                     <div>{anime.title.english ?? anime.title.romaji}</div>
                                 </a>
                             </div>
@@ -150,7 +169,7 @@
                 {/if}
             {:else}
                 No anime found.
-            {/if} -->
+            {/if}
         {/if}
     </div>
 </section>
